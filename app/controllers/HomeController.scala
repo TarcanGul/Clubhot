477953ca62d7d24123adf7d2c1c7293619965603
Tarcan Gul <nailtarcan@gmail.com>
2024-04-14 14:44:43 -0400

    refactor: remove new

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -57,7 +57,7 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents, v
                     sc.searchTrack(s"\"${track._1}\" \"${track._2}\"").map {
                       optionValue => {
                         println("Loaded track " + loadedTrackNumber.incrementAndGet() + "/100")
-                        new SpotifyTrackResult(optionValue.getOrElse(Json.parse("""{"uri" : "No song found."}"""))("uri").as[String],
+                        SpotifyTrackResult(optionValue.getOrElse(Json.parse("""{"uri" : "No song found."}"""))("uri").as[String],
                           optionValue.getOrElse(Json.parse("""{"id" : "No song found."}"""))("id").as[String],
                           optionValue.getOrElse(Json.parse("""{"name" : "No song found."}"""))("name").as[String],
                           optionValue.getOrElse(Json.parse(""" { "artists" : [] }"""))("artists").as[List[JsValue]].map(value => value("name").as[String]))
